b49f52a7e361f823cb75a88e9d68f190fe56c7a5
Boris Dibon <boris.dibon@gmail.com>
2022-01-09 12:25:05 +0100

    Dynamically render the movie details element

---
+++ scripts/components/movieDetails.js
@@ -0,0 +1,81 @@
+export const movieDetailsInnerHTML = ({
+  title,
+  image_url,
+  genres,
+  date_published,
+  rated,
+  imdb_score,
+  directors,
+  actors,
+  duration,
+  countries,
+  usa_gross_income,
+  worldwide_gross_income,
+  long_description,
+}) => {
+  const html = `
+    <div class="movie-details__grid">
+      <h2 class="movie-details__title">${title}</h2>
+      <img
+        class="movie-details__poster"
+        src="${image_url}"
+        alt="${title} film poster"
+      />
+      <div class="movie-details__text">
+        <p>
+          Genres:
+          ${genres.reduce((list, genre) => {
+            if (list.length === 0) return genre;
+            list += ", " + genre;
+            return list;
+          }, "")}
+        </p>
+        <p>Release date: ${date_published}</p>
+        <p>Rated: ${rated}</p>
+        <p>IMDb score: ${imdb_score}</p>
+        <p>
+          Directors:
+          ${directors.reduce((list, director) => {
+            if (list.length === 0) return director;
+            list += ", " + director;
+            return list;
+          }, "")}
+        </p>
+        <p>
+          Actors:
+          ${actors.reduce((list, actor) => {
+            if (list.length === 0) return actor;
+            list += ", " + actor;
+            return list;
+          }, "")}
+        </p>
+        <p>Duration: ${duration} minutes</p>
+        <p>
+          Countries:
+          ${countries.reduce((list, country) => {
+            if (list.length === 0) return country;
+            list += ", " + country;
+            return list;
+          }, "")}
+        </p>
+        <div class="movie-details__box-office">
+          Box office:
+          <ul>
+            <li>Gross US: ${
+              usa_gross_income === null ? "not available" : usa_gross_income
+            }</li>
+            <li>Gross worldwide: ${
+              worldwide_gross_income === null
+                ? "not available"
+                : worldwide_gross_income
+            }</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <p class="movie-details__description">
+      ${long_description}
+    </p>`;
+
+  return html;
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { getBestRatedMovies, getMovieDetails } from "./OCMoviesAPI.js";
 import { bestMovieHTML } from "./components/bestMovie.js";
+import { movieDetailsInnerHTML } from "./components/movieDetails.js";
 
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const nav = document.querySelector(".nav");
@@ -22,6 +23,19 @@ function insertBestMovieElement({ title, image_url, description }) {
   wrapper.innerHTML += html;
 }
 
+function updateMovieDetailsElement(movieDetails) {
+  const html = movieDetailsInnerHTML(movieDetails);
+  const isEmpty =
+    !modalInner.lastElementChild.classList.contains("movie-details");
+
+  if (!isEmpty) modalInner.lastElementChild.remove();
+
+  const newMovieDetails = document.createElement("article");
+  newMovieDetails.classList.add("movie-details");
+  newMovieDetails.innerHTML = html;
+  modalInner.appendChild(newMovieDetails);
+}
+
 (async function main() {
   const movies = await getBestRatedMovies({ page_size: 8 });
   const [bestMovie, ...veryGoodMovies] = movies;
@@ -29,9 +43,23 @@ function insertBestMovieElement({ title, image_url, description }) {
 
   console.log({ bestMovieDetails });
   insertBestMovieElement(bestMovieDetails);
+
   const bestMovieButton = document.querySelector(".best-movie__btn");
   bestMovieButton.addEventListener("click", () => {
+    updateMovieDetailsElement(bestMovieDetails);
     modalOuter.classList.toggle("open");
+
     console.dir(modalInner);
+    console.dir(closeButton);
   });
+
+  // Things to do:
+  // 1. Dynamically populate the modal (i.e the movie-details, append/remove)
+  // 2. Create the "caroussel" component (html + css)
+  // 3. Populate it dynamically
+  // 4. Bind each item to an appropriate modal
+  // 5. Repeat X times for the categories
+  // 6. Refacto
+  // 7. README
+  // 8. DONE / test production build?! Don't forget postcss autofixer
 })();
